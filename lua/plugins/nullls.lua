f491bcd043824a23b54e93b0f0b86490993d62f3
turkus <wojciech@eventory.cc>
2024-12-14 20:32:21 +0100

    ESLint

---
+++ lua/plugins/nullls.lua
@@ -0,0 +1,17 @@
+return { 
+  "jose-elias-alvarez/null-ls.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.diagnostics.eslint_d.with({
+          filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+        }),
+        null_ls.builtins.formatting.eslint_d.with({
+          filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+        }),
+      },
+    })
+  end,
+}
