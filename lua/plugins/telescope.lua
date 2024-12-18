1146f2690eb36c099c713b9bad704d71ea3f0e36
turkus <wojciech@eventory.cc>
2024-12-18 13:10:36 +0100

    Find word

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = 'Telescope find string' })
 
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
