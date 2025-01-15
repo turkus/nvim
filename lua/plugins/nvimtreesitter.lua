864da8e69875d67732ab556a1f8bfaa5ed9f558d
turkus <wojciech@eventory.cc>
2025-01-16 00:18:30 +0100

    Range selection + telescope finder of that selection

---
+++ lua/plugins/nvimtreesitter.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function () 
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "typescript", "javascriptreact", "typescriptreact", "html" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },  
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<CR>',
+            scope_incremental = '<CR>',
+            node_incremental = '<TAB>',
+            node_decremental = '<S-TAB>',
+          },
+        },
+      })
+  end
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,28 @@
 local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
 
+-- Function to search the visual selection
+local function telescope_grep_visual()
+  -- Get the visual selection
+  local start_pos = vim.fn.getpos("'<")
+  local end_pos = vim.fn.getpos("'>")
+  local lines = vim.fn.getline(start_pos[2], end_pos[2])
+
+  -- Extract the selected text
+  lines[1] = string.sub(lines[1], start_pos[3], -1)
+  lines[#lines] = string.sub(lines[#lines], 1, end_pos[3])
+  local search_text = table.concat(lines, "\n")
+
+  -- Call Telescope with the selected text
+  builtin.grep_string({ search = search_text })
+end
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = 'Telescope find string' })
+vim.keymap.set('v', '<leader>fw', telescope_grep_visual, { desc = 'Search visual selection' })
 
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
