77fa2631e4e59c57245c174848ec42ba63288394
turkus <wojciechrola@gmail.com>
2025-05-29 15:22:43 +0200

    Disable ts-tools + neotree modifications

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.api.nvim_set_keymap("n", "<Leader>e", ":Neotree reveal<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>e", ":Neotree reveal<CR>", { desc = "Reveal file in Neo-tree" })
 
 local telescope_builtin = require("telescope.builtin")
 
