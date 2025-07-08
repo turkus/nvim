8eb838229923efcf1bd0aefd1d06990686e095ca
turkus <wojciechrola@gmail.com>
2025-07-08 21:08:05 +0200

    Selection options

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,30 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = "<C-s>",
+        node_decremental = "<C-backspace>",
+      },
+    },
+  },
+}
