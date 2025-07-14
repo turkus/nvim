6b5f586f7c5d148c047fdd1b845177ad92bc3fb5
turkus <wojciechrola@gmail.com>
2025-07-14 14:34:30 +0200

    neo-tree reveal file keymap bug

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>e", ":Neotree reveal<CR>", { desc = "Reveal file in Neo-tree" })
-
 local telescope_builtin = require("telescope.builtin")
 
 local function getVisualSelection()
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,9 +7,6 @@ return {
           hide_dotfiles = false,
           hide_gitignored = true,
         },
-        follow_current_file = {
-          enabled = true,
-        },
         window = {
           mappings = {
             ["/"] = "noop",
@@ -21,5 +18,9 @@ return {
       },
       close_if_last_window = false,
     })
+
+    vim.keymap.set("n", "<leader>e", function()
+      require("neo-tree.command").execute({ action = "show", source = "filesystem", reveal = true })
+    end, { desc = "Reveal file in Neo-tree" })
   end,
 }
