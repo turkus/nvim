533387933d9f1326dceea842bc446cc663c835e7
turkus <wojciechrola@gmail.com>
2025-11-17 00:29:36 +0100

    nvim 0.11.5 + opencode

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,17 +1,20 @@
-return {
-  "jose-elias-alvarez/null-ls.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.diagnostics.eslint_d.with({
-          filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-        }),
-        null_ls.builtins.formatting.eslint_d.with({
-          filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-        }),
-      },
-    })
-  end,
-}
+-- null-ls plugin is disabled
+return {}
+
+-- return {
+--   "jose-elias-alvarez/null-ls.nvim",
+--   dependencies = { "nvim-lua/plenary.nvim" },
+--   config = function()
+--     local null_ls = require("null-ls")
+--     null_ls.setup({
+--       sources = {
+--         null_ls.builtins.diagnostics.eslint_d.with({
+--           filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+--         }),
+--         null_ls.builtins.formatting.eslint_d.with({
+--           filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+--         }),
+--       },
+--     })
+--   end,
+-- }
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,30 +1,24 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "bash",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "query",
-      "regex",
-      "tsx",
-      "typescript",
-      "vim",
-      "yaml",
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = "<C-s>",
-        node_decremental = "<C-h>",
+  -- Override LazyVim's build function to skip CLI installation
+  build = ":TSUpdate",
+  -- Use function to completely override ensure_installed
+  opts = function()
+    return {
+      -- Empty to prevent any parser installation
+      ensure_installed = {},
+      auto_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = "<C-s>",
+          node_decremental = "<C-h>",
+        },
       },
-    },
-  },
+    }
+  end,
 }
